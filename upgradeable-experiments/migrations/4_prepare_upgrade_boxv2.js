0d4578c711183355f1b8e36f3ec806c22a22c903
dmdv <dima.dyachkov@gmail.com>
2020-12-29 11:18:11 +0300

    Added more examplary test for BoxV2

--- upgradeable-experiments/migrations/4_prepare_upgrade_boxv2.js
+++ upgradeable-experiments/migrations/4_prepare_upgrade_boxv2.js
@@ -8,3 +8,14 @@ module.exports = async function (deployer) {
     const box = await Box.deployed();
     await prepareUpgrade(box.address, BoxV2, { deployer });
 };
+
+// const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+//
+// const Box = artifacts.require('Box');
+// const BoxV2 = artifacts.require('BoxV2');
+//
+// module.exports = async function (deployer) {
+//     const existing = await Box.deployed();
+//     const instance = await upgradeProxy(existing.address, BoxV2, { deployer });
+//     console.log("Upgraded", instance.address);
+// };
